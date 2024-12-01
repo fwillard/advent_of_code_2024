27c7dac6d7fcd94cd2f4a53cfe0d4ef00216b30f
Finn Willard <finn.willard@gmail.com>
2024-12-01 10:10:09 -0800

    part 2 passing

--- 01/01.py
+++ 01/01.py
@@ -8,4 +8,15 @@ df["dist"] = abs(df["L1"] - df["L2"])
 
 sum = df["dist"].sum()
 
-print(sum)
+print(f"distance: {sum}")
+
+counts = df["L2"].value_counts()
+
+df["similarity"] = df["L1"] * df["L1"].map(counts)
+
+sum = int(df["similarity"].sum())
+
+print(f"similarity: {sum}")
+
+
+# similarity
