b97da3f8d11304488599ae3cb565d1a9aa4ddeac
Finn Willard <finn.willard@gmail.com>
2024-12-03 07:30:26 -0800

    part 2 solution

--- 03/03.py
+++ 03/03.py
@@ -9,3 +9,17 @@ for m in matches:
     total += int(m[0]) * int(m[1])
 
 print(f"part 1: {total}")
+
+matches = re.findall(r"mul\((\d+),(\d+)\)|(do)\(\)|(don't)\(\)", data)
+
+enabled = True
+total = 0
+for m in matches:
+    if "do" in m:
+        enabled = True
+    elif "don't" in m:
+        enabled = False
+    elif enabled:
+        total += int(m[0]) * int(m[1])
+
+print(f"part 2: {total}")
